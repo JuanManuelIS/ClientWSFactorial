06a97673afdb7852f17706f72fa00b89bb4b0afc
Entrar <Entrar>
2018-03-13 12:56:46 +0000

    First commit

---
+++ build/generated-sources/jax-ws/service/Factorial.java
@@ -0,0 +1,52 @@
+
+package service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para factorial complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="factorial">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="number" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "factorial", propOrder = {
+    "number"
+})
+public class Factorial {
+
+    protected int number;
+
+    /**
+     * Obtiene el valor de la propiedad number.
+     * 
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Define el valor de la propiedad number.
+     * 
+     */
+    public void setNumber(int value) {
+        this.number = value;
+    }
+
+}
